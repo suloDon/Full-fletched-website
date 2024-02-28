34f5cd8418b34df83c85d22910a0a9169bcfa29c
Suraj Thapa <surajthapart@gmail.com>
2024-02-28 22:56:17 +0545

    Added page 3 with some java script

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+
+let popup = document.querySelector(".hover");
+
+let show = document.querySelector(".fixed-image")
+
+popup.addEventListener("mouseenter", ()=>{
+    show.style.display = "block"
+})
+
+popup.addEventListener("mouseleave", ()=>{
+    show.style.display = "none"
+})
+
+
+let items = document.querySelectorAll(".items")
+items.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
+        let image = e.getAttribute("data-image")
+        show.style.backgroundImage = `url(${image})`;
+        console.log(image)
+    })
+    
+})
